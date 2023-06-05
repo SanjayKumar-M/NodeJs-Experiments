4adaf83789f43df7df8656ddbb2d067a803f9a6f
SanjayKumar-M <sanjaymoad786@gmail.com>
2023-06-05 11:17:06 +0530

    Implemented server

--- Server.js
+++ Server.js
@@ -3,14 +3,17 @@ import fs from 'fs'
 const Server = http.createServer((req, res) => {
 
     res.writeHead(200, { "Content-Type": "Text/html" })
-    let html =  fs.readFileSync('./html/index.html','utf-8')
+    let html = fs.readFileSync('./html/index.html', 'utf-8')
+    process.nextTick(() => {
+        console.log("Learning Event loop")
+    })
     let user = "Sanjay"
     //using custom values in html with replace()
 
     html = html.replace('{name}', user)
     res.end(html)
 
-    
+
 })
 
 
