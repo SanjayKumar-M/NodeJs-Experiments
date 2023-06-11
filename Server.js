81248b7bf15fe2324eaa0a3e6e2895da4dd7ab3c
SanjayKumar-M <sanjaymoad786@gmail.com>
2023-06-11 20:17:28 +0530

    update

--- Server.js
+++ Server.js
@@ -1,24 +1,44 @@
-import http from 'http'
-import fs from 'fs'
-import fileOperations from './Concepts/filePromises.js'
-const Server = http.createServer((req, res) => {
+import Express  from "express";
+import os from 'os'
+import axios from "axios";
+const app = Express();
+const port = 3000;
 
-    res.writeHead(200, { "Content-Type": "Text/html" })
-    let html = fs.readFileSync('./html/index.html', 'utf-8')
-    process.nextTick(() => {
-        console.log("Learning Event loop")
-    })
-    let user = "Sanjay"
-    //using custom values in html with replace()
-
-    html = html.replace('{name}', user)
-    res.end(html)
-
-
+app.get('/api/data', (req, res) => {
+   res.send(os.cpus())
 })
 
-fileOperations()
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
 
-Server.listen(3000, () => { console.log("Server connection established Successfully...") })
+// server.get('/:id',(req,res)=>{
+//     const {id} = req.params
+
+//     res.send(`User name is ${id}`)
+// })
+
+// import http from 'http'
+// import fs from 'fs'
+// import fileOperations from './Concepts/filePromises.js'
+// const Server = http.createServer((req, res) => {
+
+//     res.writeHead(200, { "Content-Type": "Text/html" })
+//     let html = fs.readFileSync('./html/index.html', 'utf-8')
+//     process.nextTick(() => {
+//         console.log("Learning Event loop")
+//     })
+//     let user = "Sanjay"
+//     //using custom values in html with replace()
+
+//     html = html.replace('{name}', user)
+//     res.end(html)
+
+
+// })
+
+// fileOperations()
+
+// Server.listen(3000, () => { console.log("Server connection established Successfully...") })
 
 
