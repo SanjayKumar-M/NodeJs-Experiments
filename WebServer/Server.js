8ebb710cb1e5f2baf628341987b593138587a87a
SanjayKumar-M <sanjaymoad786@gmail.com>
2023-08-15 16:47:18 +0530

    server

--- WebServer/Server.js
+++ WebServer/Server.js
@@ -1,9 +1,33 @@
-import fs from 'fs/promises'
+import http from 'http'
 
-const File = async() =>{
-    const readData = await fs.readFile('haha.txt','utf-8')
-    console.log(readData) 
+const Server = http.createServer((req, res) => {
+    switch (req.method) {
+        case "GET":
+            res.statusCode = 200;
+            res.setHeader("Content-Type", "text/plain");
+            res.end("GET request received");
+            break;
+        case "POST":
+            res.statusCode = 200;
+            res.setHeader("Content-Type", "text/plain");
+            res.end("POST request received");
+            break;
+        case "PUT":
+            res.statusCode = 200;
+            res.setHeader("Content-Type", "text/plain");
+            res.end("PUT request received");
+            break;
+        case "DELETE":
+            res.statusCode = 200;
+            res.setHeader("Content-Type", "text/plain");
+            res.end("DELETE request received");
+            break;
 
-    await fs.writeFile('./Hello.txt',"Hi I'm Sanjay")
-}
-File()
+        default:
+            res.statusCode = 404;
+            res.setHeader("Content-Type", "text/plain");
+            res.end("No Method found")
+    }
+})
+
+Server.listen(3000, () => { console.log("Listening...") })
